49cea24f5f51aa3f379140222c9029a043155fa0
Cristian Romero <crismaro1997.19@gmail.com>
2025-01-04 18:55:20 -0500

    Se inician con los ejercicios

---
+++ 03-control-flujo/02-else.js
@@ -0,0 +1,9 @@
+let edad = 10;
+
+if (edad > 17) {
+    console.log('usuario mayor de edad');
+} else if(edad > 13){
+    console.log('usuario necesita estar con adulto responsable');
+} else {
+    console.log('usuario menor de edad');
+}
